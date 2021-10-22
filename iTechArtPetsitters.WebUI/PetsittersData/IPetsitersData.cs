e7db2d0cfbf3a629bf3fa3bfa2a99573f0e7477b
iailyas <62544436+iailyas@users.noreply.github.com>
2021-10-23 01:40:25 +0300

    Добавьте файлы проекта.

---
+++ iTechArtPetsitters.WebUI/PetsittersData/IPetsitersData.cs
@@ -0,0 +1,22 @@
+﻿using iTechArtPetsitters.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iTechArtPetsitters.WebUI.PetsittersData
+{
+    public interface IPetsitersData
+    {
+        List<Petsitter> GetPetsitters();
+
+        Petsitter GetPetsitter(Guid id);
+
+        Petsitter AddPetsitter(Petsitter Petsitter);
+
+        void DeletePetsitter(Petsitter Petsitter);
+
+        Petsitter EditPetsitter(Petsitter petsitter);
+
+    }
+}
