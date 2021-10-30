2dfef4f8ef025a516442a92fcb0878c252129bb8
iailyas <62544436+iailyas@users.noreply.github.com>
2021-10-30 16:24:30 +0300

    AddOnionArchitecture

---
+++ README.md
@@ -0,0 +1,2 @@
+# iTechArtPetsitters.WebUI
+![BD](https://user-images.githubusercontent.com/62544436/139104079-c0b86612-c2bf-43b3-af51-ec78ca271b24.PNG)
---
+++ iTechArtPetsitters.WebUI/Controllers/PetsittersController.cs
@@ -0,0 +1,83 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iTechArtPetsitters.WebUI.Controllers
+{
+    
+    [ApiController]
+
+    
+    public class PetsittersController : ControllerBase
+    {
+        private IPetsitersData _petsitterData;
+        public PetsittersController(IPetsitersData petsitterData) 
+        {
+            _petsitterData = petsitterData;
+        }
+        [HttpGet]
+        [Route("api/[controller]")]
+
+        public IActionResult GetPetsitters()
+        {
+            return Ok(_petsitterData.GetPetsitters());
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+
+        public IActionResult GetPetsitter(Guid id)
+        {
+            var petsitter = _petsitterData.GetPetsitter(id);
+            if (petsitter != null) 
+            {
+             return Ok(petsitter);
+            }
+            return NotFound($"Employee with id: {id} not found");
+            
+        }
+
+        [HttpPost]
+        [Route("api/[controller]")]
+
+        public IActionResult AddPersitter(Petsitter petsitter)
+        {
+            _petsitterData.AddPetsitter(petsitter);
+            return Created(HttpContext.Request.Scheme+"://"+HttpContext.Request.Host+HttpContext.Request.Path+"/"+petsitter.Id,petsitter);
+
+        }
+
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+
+        public IActionResult DeletePersitter(Guid id)
+        {
+            var petsitter=_petsitterData.GetPetsitter(id);
+            if (petsitter != null)
+            {
+                _petsitterData.DeletePetsitter(petsitter);
+            }
+            return NotFound($"Employee with id: {id} not found");
+
+        }
+        [HttpPatch]
+        [Route("api/[controller]/{id}")]
+
+        public IActionResult EditPersitter(Guid id,Petsitter petsitter)
+        {
+            var existingPetsitter = _petsitterData.GetPetsitter(id);
+            if (existingPetsitter != null)
+            {
+                petsitter.Id = existingPetsitter.Id;
+                _petsitterData.EditPetsitter(petsitter);
+            }
+            return NotFound($"Employee with id: {id} not found");
+
+        }
+
+    }
+}
