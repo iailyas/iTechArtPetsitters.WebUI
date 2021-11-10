acaefbf847d3a53e58cf85fcf8fba1c73fe27cb3
iailyas <62544436+iailyas@users.noreply.github.com>
2021-11-10 17:34:55 +0300

    before debugging

--- iTechArtPetsitters.WebUI/Controllers/UserController.cs
+++ iTechArtPetsitters.WebUI/Controllers/UserController.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 
 namespace iTechArtPetsitters.WebUI.Controllers
 {
+    
     [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
         IUserRepository UserRepository;
@@ -21,7 +23,7 @@ namespace iTechArtPetsitters.WebUI.Controllers
             return UserRepository.Get();
         }
         //returns user by id
-        [HttpGet("{id}", Name = "GetUser")]
+        [HttpGet("{id::long}", Name = "GetUser")]
         public IActionResult Get(long Id)
         {
             MyUser user = UserRepository.Get(Id);
@@ -42,10 +44,10 @@ namespace iTechArtPetsitters.WebUI.Controllers
                 return BadRequest();
             }
             UserRepository.Create(user);
-            return CreatedAtRoute("GetTodoItem", new { id = user.id }, user);
+            return CreatedAtRoute("GetUser", new { id = user.id }, user);
         }
         //replaces all records with data from request
-        [HttpPut("{id}")]
+        [HttpPut("{id::long}")]
         public IActionResult Update(long Id, [FromBody] MyUser updatedUser)
         {
             if (updatedUser == null || updatedUser.id != Id)
@@ -63,7 +65,7 @@ namespace iTechArtPetsitters.WebUI.Controllers
             return RedirectToRoute("GetAllUsers");
         }
         //delete record by id
-        [HttpDelete("{id}")]
+        [HttpDelete("{id::long}")]
         public IActionResult Delete(long Id)
         {
             var deletedUser = UserRepository.Delete(Id);
